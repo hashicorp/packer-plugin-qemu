4d2f258919117b5db967b84fd3ff110bb1c7a2ba
Adrien Delorme <azr@users.noreply.github.com>
2021-08-31 15:57:16 +0200

    consolidate version location

--- main.go
+++ main.go
@@ -4,29 +4,16 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/hashicorp/packer-plugin-qemu/builder/qemu"
 	"github.com/hashicorp/packer-plugin-sdk/plugin"
-	"github.com/hashicorp/packer-plugin-sdk/version"
-)
 
-var (
-	// Version is the main version number that is being run at the moment.
-	Version = "1.0.1"
-
-	// VersionPrerelease is A pre-release marker for the Version. If this is ""
-	// (empty string) then it means that it is a final release. Otherwise, this
-	// is a pre-release such as "dev" (in development), "beta", "rc1", etc.
-	VersionPrerelease = "dev"
-
-	// PluginVersion is used by the plugin set to allow Packer to recognize
-	// what version this plugin is.
-	PluginVersion = version.InitializePluginVersion(Version, VersionPrerelease)
+	"github.com/hashicorp/packer-plugin-qemu/builder/qemu"
+	"github.com/hashicorp/packer-plugin-qemu/version"
 )
 
 func main() {
 	pps := plugin.NewSet()
 	pps.RegisterBuilder(plugin.DEFAULT_NAME, new(qemu.Builder))
-	pps.SetVersion(PluginVersion)
+	pps.SetVersion(version.PluginVersion)
 	err := pps.Run()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
