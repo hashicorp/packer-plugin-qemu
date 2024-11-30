1e6d18e3dc552f2451a351b4ff2e410e1376da3b
Carlos Nunez <13461447+carlosonunez@users.noreply.github.com>
2024-11-30 16:09:06 -0600

    CORRECTLY add vnc_password to hcl2 spec
    Signed-off-by: Carlos Nunez <13461447+carlosonunez@users.noreply.github.com>

--- builder/qemu/config.hcl2spec.go
+++ builder/qemu/config.hcl2spec.go
@@ -136,8 +136,8 @@ type FlatConfig struct {
 	VGA                       *string           `mapstructure:"vga" required:"false" cty:"vga" hcl:"vga"`
 	Display                   *string           `mapstructure:"display" required:"false" cty:"display" hcl:"display"`
 	VNCBindAddress            *string           `mapstructure:"vnc_bind_address" required:"false" cty:"vnc_bind_address" hcl:"vnc_bind_address"`
+	VNCPassword               *string           `mapstructure:"vnc_password" required:"false" cty:"vnc_password" hcl:"vnc_password"`
 	VNCUsePassword            *bool             `mapstructure:"vnc_use_password" required:"false" cty:"vnc_use_password" hcl:"vnc_use_password"`
-	VNCPassword               *bool             `mapstructure:"vnc_password" required:"false" cty:"vnc_password" hcl:"vnc_password"`
 	VNCPortMin                *int              `mapstructure:"vnc_port_min" required:"false" cty:"vnc_port_min" hcl:"vnc_port_min"`
 	VNCPortMax                *int              `mapstructure:"vnc_port_max" cty:"vnc_port_max" hcl:"vnc_port_max"`
 	VMName                    *string           `mapstructure:"vm_name" required:"false" cty:"vm_name" hcl:"vm_name"`
@@ -288,6 +288,7 @@ func (*FlatConfig) HCL2Spec() map[string]hcldec.Spec {
 		"vga":                          &hcldec.AttrSpec{Name: "vga", Type: cty.String, Required: false},
 		"display":                      &hcldec.AttrSpec{Name: "display", Type: cty.String, Required: false},
 		"vnc_bind_address":             &hcldec.AttrSpec{Name: "vnc_bind_address", Type: cty.String, Required: false},
+		"vnc_password":                 &hcldec.AttrSpec{Name: "vnc_password", Type: cty.String, Required: false},
 		"vnc_use_password":             &hcldec.AttrSpec{Name: "vnc_use_password", Type: cty.Bool, Required: false},
 		"vnc_port_min":                 &hcldec.AttrSpec{Name: "vnc_port_min", Type: cty.Number, Required: false},
 		"vnc_port_max":                 &hcldec.AttrSpec{Name: "vnc_port_max", Type: cty.Number, Required: false},
